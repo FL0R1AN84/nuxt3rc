d328a9fc122c57a43d23f2a8e3771bfe8267dbc2
Florian Wächter <waechter@standpunkt.com>
2022-11-21 22:15:17 +0100

    Add components and composables

---
+++ components/First.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>I am a component</div>
+  <Second />
+</template>
+
+<script setup lang="ts">
+  import Second from "~~/composables/Second.vue";
+</script>
+
+<style scoped></style>
---
+++ composables/Second.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>I am a composables</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
